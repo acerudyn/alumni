24ba4e1413044370d3edd36f0404cd8292c4fcc6
acerudyn <acerudyn@yahoo.com>
2018-08-07 22:00:07 +0700

    update index & style

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 <script src="js/lightbox/jquery.lightbox.min.js" type="text/javascript"></script>
 	<script type="text/javascript">
 		$(document).ready(function() {
-		    $('.lightbox').lightbox();		    
+		    $('.lightbox').lightbox();
 		});
   </script>
 
@@ -21,27 +21,27 @@
 <body>
 
 <div id="wrapper">
-	
-			
+
+
 <div id="head" class="clearfloat">
   <div class="clearfloat">
-  
-		
-      <div class="right"> <img src="images/ads/HD.jpg" alt="" width="960" height="150" /> </div>
+
+
+      <div class="right"> <img src="images/ads/HD.png" alt="" width="960" height="190" /> </div>
   </div>
-		
+
   <div id="navbar" class="clearfloat">
     <ul id="page-bar" class="left clearfloat">
       <li><a href="?t=home">Home</a></li>
 	  <li><a href="">Informasi sekolah</a>
 			<ul>
 				<li><a href="?t=visimisi"><span>Visi & Misi</a></span></li>
-				<li><a href="?t=tujuan"><span>Tujuan</a></span></li>					
+				<li><a href="?t=tujuan"><span>Tujuan</a></span></li>
 				<li><a href="?t=sejarah"><span>sejarah</span></a></li>
 				<li><a href="?t=struktur"><span>Struktur</span></a></li>
-				<li><a href="?t=fasilitas"><span>Fasilitas</a></span></li>	
-				<li><a href="?t=ekstra"><span>Ekstrakurikuler</a></span></li>	
-				<li><a href="?t=alamat"><span>Suasana Sekolah</a></span></li>	
+				<li><a href="?t=fasilitas"><span>Fasilitas</a></span></li>
+				<li><a href="?t=ekstra"><span>Ekstrakurikuler</a></span></li>
+				<li><a href="?t=alamat"><span>Suasana Sekolah</a></span></li>
 			</ul>
 			</li>
 				<li><a href="?t=berita">Berita</a>
@@ -58,16 +58,16 @@
 				<li><a href="?t=profilalumni">Alumni</a></li>
 				<li><a href="?t=kontak kami">Kontak kami</a></li>
       </ul>
-	  
-	  
+
+
     <form method="get" id="searchform" action="http://www.work.baleho.com/">
-      
+
     </form>
   </div>
 </div>
 <div id="page" class="clearfloat">
   <div id="top" class="clearfloat">
-    <div id="headline"> 
+    <div id="headline">
       <div class="title"></div>
       <div class="meta"></div>
       <?php
@@ -78,7 +78,7 @@
     } else if (isset($_GET['t']) && $_GET['t'] == 'visimisi') {
     	include 'menu/informasi/visimisi.php';
 	} else if (isset($_GET['t']) && $_GET['t'] == 'tujuan') {
-    	include 'menu/informasi/tujuan.php';	
+    	include 'menu/informasi/tujuan.php';
 	} else if (isset($_GET['t']) && $_GET['t'] == 'sejarah') {
     	include 'menu/informasi/sejarah.php';
 	} else if (isset($_GET['t']) && $_GET['t'] == 'struktur') {
@@ -94,15 +94,15 @@
 	} else if (isset($_GET['t']) && $_GET['t'] == 'suasana') {
     	include 'menu/suasana/suasana.php';
 	} else if (isset($_GET['t']) && $_GET['t'] == 'pengembangan') {
-    	include 'menu/pengembangan/pengembangan.php';	
+    	include 'menu/pengembangan/pengembangan.php';
 	} else if (isset($_GET['t']) && $_GET['t'] == 'lowongan') {
-    	include 'menu/lowongan/lowongan.php';	
+    	include 'menu/lowongan/lowongan.php';
 	} else if (isset($_GET['t']) && $_GET['t'] == 'acara') {
     	include 'menu/acara/acara.php';
     } else if (isset($_GET['t']) && $_GET['t'] == 'galery') {
     	include 'menu/galery/galery.php';
 	} else if (isset($_GET['t']) && $_GET['t'] == 'kontak kami') {
-    	include 'menu/informasi/kontak.php';	
+    	include 'menu/informasi/kontak.php';
     }  else if (isset($_GET['t']) && $_GET['t'] == 'detailgalery') {
     	include 'menu/galery/detailgalery.php';
     } else if (isset($_GET['t']) && $_GET['t'] == 'profilalumni') {
@@ -124,9 +124,9 @@
 	}
     ?>
        </div>
-		 
+
 	<div id="sidebar">
-     
+
      <div id="sidebar-top">
         <center><h3>Login Admin</h3><center>
         <p><a href="admin/index.php">::::LOG IN::::</a></p>
@@ -137,60 +137,60 @@
         <li>
       <hr size="1"/>
       <?php
-                        
+
                         include 'res/koneksi.php';
 						$sql=mysql_query("select * from beasiswa  order by id_beasiswa desc limit 5 ");
-						while($s=mysql_fetch_array($sql)){	
+						while($s=mysql_fetch_array($sql)){
 						?>
                             <p><a href="admin/dokumen/<?php echo $s['file'] ?>"><?php echo $s['nama_beasiswa']?></a></p>
-                            
-						<?php	
-						
+
+						<?php
+
 						}
 				?>
       </li>
         </p>
         </div> -->
-		
+
 		<div id="sidebar-bottom">
         <center><h3>Download Formulir Lowongan Kerja</h3></center>
         <p><?php include "menu/lowongan/download_lowongan.php";?></p>
         </div>
-		
+
 		<div id="sidebar-bottom">
         <center><h3>Forum Facebook</h3></center>
 		<center><li><a href='http://facebook.com/pages/SMAN1CIKARANGSELATAN'><img src="images/facebook.png" /></a></li></center>
 		</div>
-		
+
 		<div id="sidebar-bottom">
 		<center><h3>Kalender</h3></center>
 		<div class="c" align="center">
 		<?php include "kalender.php"?>
 		</div>
   </div>
-</div>	 
-	
+</div>
+
   <div id="bottom" class="clearfloat">
-    
+
       <div class="clearfloat">
         <!--<h3 class="cat_title">GALERY&raquo;</h3>
-        
+
         <p> <?php include "menu/galery/galery.php" ?></p>
       </div>
       <div class="divider"><img src="images/divider.png" alt="" /></div>
       <div class="clearfloat">
         <h3 class="cat_title">Info Lowongan&raquo;</h3>
-			
+
         -->
        <!-- <p> <?php include "menu/lowongan/lowongan.php" ?></p> -->
       </div>
       <div class="divider"></div>
       <div class="navigation">
-        
+
         <div class="right"></div>
       </div>
     </div>
-    
+
 <div id="front-popular" class="clearfloat">
   <div id="wrp5">
 					<div id="wrp5_inner">
@@ -205,14 +205,14 @@
 	EMAIL : smkn_01ciksel@yahoo.com<br>
 
 	</p>		</div>
-	
-						</div>						
+
+						</div>
 																								<div class="clrfix"></div>
-					</div>	
+					</div>
 			</div>
 
-   
-  
-    
+
+
+
 </body>
 </html>
